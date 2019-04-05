77d779c15348b7c01b6242a505bcf8ad4f200407
yuebinyun <yuebinyun@gmail.com>
2019-04-06 00:15:52 +0800

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,42 @@
+import net.coobird.thumbnailator.Thumbnails;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.List;
+
+@SuppressWarnings("all")
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        List<BufferedImage> images = Thumbnails
+                .of(new File("sample").listFiles())
+                // 对图片进行裁剪，去掉四周的黑框，然后放大一倍。
+                .sourceRegion(1, 1, 88, 18)
+                .size(180, 40)
+                .outputFormat("jpg")
+                .asBufferedImages();
+
+        ITesseract instance = new Tesseract();
+        instance.setDatapath(".");
+        instance.setLanguage("eng");
+
+        for (BufferedImage image : images) {
+            String result = instance.doOCR(image).substring(0, 3);
+            int a = Integer.valueOf(String.valueOf(result.charAt(0)));
+            char op = result.charAt(1);
+            int b = Integer.valueOf(String.valueOf(result.charAt(2)));
+            int ret = 9999;
+            if (op == 'X') {
+                ret = a * b;
+            } else if (op == '+') {
+                ret = a + b;
+            } else if (op == '-') {
+                ret = a - b;
+            }
+            System.out.println(result + "=" + ret);
+        }
+    }
+}
