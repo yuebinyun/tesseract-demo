17e5570e5b008d854b0ed61f3d5709c9d89b5410
yuebinyun <yuebinyun@gmail.com>
2019-05-06 19:49:00 +0800

    1.增加测试图片  2.配置 Gradle 仓库 aliyun 镜像

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,9 +12,9 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         List<BufferedImage> images = Thumbnails
-                .of(new File("sample").listFiles())
+                .of(new File("sample4").listFiles())
                 // 对图片进行裁剪，去掉四周的黑框，然后放大一倍。
-                .sourceRegion(1, 1, 88, 18)
+//                .sourceRegion(1, 1, 88, 18)
                 .size(180, 40)
                 .outputFormat("jpg")
                 .asBufferedImages();
@@ -22,21 +22,25 @@ public class Main {
         ITesseract instance = new Tesseract();
         instance.setDatapath(".");
         instance.setLanguage("eng");
+//        instance.setLanguage("fontyp");
+//        instance.setLanguage("zwp");
 
         for (BufferedImage image : images) {
-            String result = instance.doOCR(image).substring(0, 3);
-            int a = Integer.valueOf(String.valueOf(result.charAt(0)));
-            char op = result.charAt(1);
-            int b = Integer.valueOf(String.valueOf(result.charAt(2)));
-            int ret = 9999;
-            if (op == 'X') {
-                ret = a * b;
-            } else if (op == '+') {
-                ret = a + b;
-            } else if (op == '-') {
-                ret = a - b;
-            }
-            System.out.println(result + "=" + ret);
+            System.out.println(
+                    instance.doOCR(image).replace("\n", ""));
+//            String result = instance.doOCR(image).substring(0, 3);
+//            int a = Integer.valueOf(String.valueOf(result.charAt(0)));
+//            char op = result.charAt(1);
+//            int b = Integer.valueOf(String.valueOf(result.charAt(2)));
+//            int ret = 9999;
+//            if (op == 'X') {
+//                ret = a * b;
+//            } else if (op == '+') {
+//                ret = a + b;
+//            } else if (op == '-') {
+//                ret = a - b;
+//            }
+//            System.out.println(result + "=" + ret);
         }
     }
 }
